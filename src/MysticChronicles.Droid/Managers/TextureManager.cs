cfdf7ea44cba29691c909392f39eddd0864d8366
Jarred Capellman <jcapellman@hotmail.com>
2017-07-10 21:23:46 -0500

    Added: Initial work on the TextureManager

---
+++ src/MysticChronicles.Droid/Managers/TextureManager.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MysticChronicles.Managers
+{
+    public class TextureManager
+    {
+        internal Texture2D LoadTexture(ContentManager content, string assetName)
+        {
+            return content.Load<Texture2D>(assetName);
+        }
+    }
+}
