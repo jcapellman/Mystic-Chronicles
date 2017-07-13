fdfad87f64dddca9ea5506ba1daf6656db345113
Jarred Capellman <jcapellman@hotmail.com>
2017-07-12 21:32:53 -0500

    Added: GameStateContainer Container Object

---
+++ src/MysticChronicles.Droid/Objects/Common/GameStateContainer.cs
@@ -0,0 +1,13 @@
+﻿using MysticChronicles.Managers;
+
+namespace MysticChronicles.Droid.Objects.Common
+{
+    public class GameStateContainer
+    {
+        public TextureManager TManager { get; set; }
+
+        public int Window_Width { get; set; }
+
+        public int Window_Height { get; set; }
+    }
+}
