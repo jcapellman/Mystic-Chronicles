fa40a972eea26c2fd52e537f8431d44646f2c356
Jarred Capellman <jcapellman@hotmail.com>
2017-07-12 20:40:17 -0500

    Added: Initial work on the MainMenuState Class

---
+++ src/MysticChronicles.Droid/GameStates/MainMenuState.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework.Content;
+
+using MysticChronicles.Managers;
+
+namespace MysticChronicles.GameStates
+{
+    public class MainMenuState : BaseGameState
+    {
+        public MainMenuState(TextureManager argTextureManager, int argWidth, int argHeight) : base(argTextureManager, argWidth, argHeight)
+        {
+        }
+
+        public override void LoadContent()
+        {
+            textures.Add(textureManager.LoadTexture("UI/MainMenu"));
+        }
+    }
+}
