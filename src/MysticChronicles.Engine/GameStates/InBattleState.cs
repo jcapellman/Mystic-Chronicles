081a4ba4af422ebfe411aacbb958476bfba4b067
Jarred Capellman <jcapellman@hotmail.com>
2017-07-15 13:00:08 -0500

    Added: InBattleState Class

---
+++ src/MysticChronicles.Engine/GameStates/InBattleState.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework.Input;
+
+using MysticChronicles.Engine.Objects.Common;
+using MysticChronicles.Engine.Objects.Element.Static;
+
+namespace MysticChronicles.Engine.GameStates
+{
+    public class InBattleState : BaseGameState
+    {
+        public InBattleState(GameStateContainer container) : base(container) { }
+
+        public override void HandleInput(GamePadState gamePadState)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void LoadContent()
+        {
+            AddGraphicElement(new BackgroundImage(EContainer, "Battle Backgrounds/Forest"));
+        }
+    }
+}
