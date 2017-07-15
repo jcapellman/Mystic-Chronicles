bc18a9816336a8e354fcdac0933afeebc440ab9d
Jarred Capellman <jcapellman@hotmail.com>
2017-07-15 16:19:13 -0500

    Added: iOS Port

---
+++ src/MysticChronciles.iOS/Program.cs
@@ -0,0 +1,30 @@
+﻿using Foundation;
+
+using UIKit;
+
+using MysticChronicles.Engine;
+
+namespace MysticChronciles.iOS
+{
+    [Register("AppDelegate")]
+    class Program : UIApplicationDelegate
+    {
+        private static MainGame game;
+
+        internal static void RunGame()
+        {
+            game = new MainGame();
+            game.Run();
+        }
+
+        static void Main(string[] args)
+        {
+            UIApplication.Main(args, null, "AppDelegate");
+        }
+
+        public override void FinishedLaunching(UIApplication app)
+        {
+            RunGame();
+        }
+    }
+}
