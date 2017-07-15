1d64daeedc757a2926b01ba769c8a9cad09d0f17
Jarred Capellman <jcapellman@hotmail.com>
2017-07-15 16:32:42 -0500

    Added: Windows Desktop Port

---
+++ src/MysticChronicles.Windows/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using MysticChronicles.Engine;
+
+namespace MysticChronicles.Windows
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new MainGame())
+                game.Run();
+        }
+    }
+}
