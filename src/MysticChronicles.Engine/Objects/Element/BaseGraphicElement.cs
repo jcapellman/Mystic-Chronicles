b7374f4f3fcad1259ed36da01e33e9fcf071a046
Jarred Capellman <jcapellman@hotmail.com>
2017-07-14 20:45:34 -0500

    Added: BaseGraphicElement Class

---
+++ src/MysticChronicles.Engine/Objects/Element/BaseGraphicElement.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+using MysticChronicles.Engine.Objects.Common;
+
+namespace MysticChronicles.Engine.Objects.Element
+{
+    public abstract class BaseGraphicElement : BaseElement
+    {
+        protected Texture2D TextureElement;
+        protected int Window_Width;
+        protected int Window_Height;
+
+        protected BaseGraphicElement(ElementContainer elementContainer, string textureName)
+        {
+            TextureElement = elementContainer.TextureManager.LoadTexture(textureName);
+
+            Window_Width = elementContainer.Window_Width;
+            Window_Height = elementContainer.Window_Height;
+        }
+
+        public abstract void Render(SpriteBatch spriteBatch);
+    }
+}
