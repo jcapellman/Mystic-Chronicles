0389a6721b7c78ceef1800af7f6aead70f24d1a0
Jarred Capellman <jcapellman@hotmail.com>
2017-07-14 20:44:55 -0500

    Added: ElementContainer Class

---
+++ src/MysticChronicles.Engine/Objects/Common/ElementContainer.cs
@@ -0,0 +1,13 @@
+﻿using MysticChronicles.Engine.Managers;
+
+namespace MysticChronicles.Engine.Objects.Common
+{
+    public class ElementContainer
+    {
+        public int Window_Width { get; set; }
+
+        public int Window_Height { get; set; }
+
+        public TextureManager TextureManager { get; set; }
+    }
+}
