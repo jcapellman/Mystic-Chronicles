db69ee19a4c4d2e6192087aeb016f7b3326d936e
Jarred Capellman <jcapellman@hotmail.com>
2017-07-16 10:16:45 -0500

    Added: Visibility support to the Text Elements

--- src/MysticChronicles.Engine/Objects/Element/Static/StaticText.cs
+++ src/MysticChronicles.Engine/Objects/Element/Static/StaticText.cs
@@ -14,13 +14,15 @@ namespace MysticChronicles.Engine.Objects.Element.Static
 
         private readonly Color _color;
 
-        public StaticText(SpriteFont font, string text, Color color, int xPosition, int yPosition)
+        public StaticText(SpriteFont font, string text, Color color, int xPosition, int yPosition, bool isVisible = true)
         {
             _font = font;
             _text = text;
             _color = color;
             _xPosition = xPosition;
             _yPosition = yPosition;
+
+            IsVisible = isVisible;
         }
 
         public void Render(SpriteBatch spriteBatch)
