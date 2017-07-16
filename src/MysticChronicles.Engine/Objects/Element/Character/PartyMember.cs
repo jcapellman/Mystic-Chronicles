15750b2387c48cba9ec40bf80905337d10d5100a
Jarred Capellman <jcapellman@hotmail.com>
2017-07-16 10:44:44 -0500

    Added: Initial work on the PartyMember Class

---
+++ src/MysticChronicles.Engine/Objects/Element/Character/PartyMember.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+using MysticChronicles.Engine.Objects.Common;
+
+namespace MysticChronicles.Engine.Objects.Element.Character
+{
+    public class PartyMember : BaseGraphicElement
+    {    
+        public PartyMember(ElementContainer elementContainer, string textureName, bool isVisible = true) : base(elementContainer, textureName, isVisible)
+        {
+        }   
+
+        public override void Render(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(TextureElement, new Rectangle(0, 0, TextureElement.Width, TextureElement.Height), Color.White);
+        }
+    }
+}
