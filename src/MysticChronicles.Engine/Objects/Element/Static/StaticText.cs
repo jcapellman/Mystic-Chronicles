7fabef81955d5676970a8a19b41b0c5aec8eb03e
Jarred Capellman <jcapellman@hotmail.com>
2017-07-16 00:20:25 -0500

    Added: StaticText Class to assist with adding text

---
+++ src/MysticChronicles.Engine/Objects/Element/Static/StaticText.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MysticChronicles.Engine.Objects.Element.Static
+{
+    public class StaticText : BaseElement
+    {
+        private readonly int _xPosition;
+        private readonly int _yPosition;
+
+        private readonly string _text;
+
+        private readonly SpriteFont _font;
+
+        private readonly Color _color;
+
+        public StaticText(SpriteFont font, string text, Color color, int xPosition, int yPosition)
+        {
+            _font = font;
+            _text = text;
+            _color = color;
+            _xPosition = xPosition;
+            _yPosition = yPosition;
+        }
+
+        public void Render(SpriteBatch spriteBatch)
+        {
+            spriteBatch.DrawString(_font, _text, new Vector2(_xPosition, _yPosition), _color);
+        }
+    }
+}
