b5ccd23acbce4c099d472429814eaf56f944f2ce
Jarred Capellman <jcapellman@hotmail.com>
2017-07-17 18:23:12 -0500

    Change: Finished refactoring the BaseGameObject

--- src/MysticChronicles.Engine/GameObjects/BaseGameObject.cs
+++ src/MysticChronicles.Engine/GameObjects/BaseGameObject.cs
@@ -1,22 +1,36 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System.Collections.Generic;
+
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 using MysticChronicles.Engine.Enums;
+
+using MysticChronicles.Engine.Objects.Common;
+using MysticChronicles.Engine.Objects.Element;
 using MysticChronicles.Engine.Objects.Element.Static;
 
 namespace MysticChronicles.Engine.GameObjects
 {
-    public class BaseGameObject
+    public abstract class BaseGameObject
     {
-        public StaticText AddText(SpriteFont font, string text, Color color, int scale, TextAlignment textAlignment, int screenWidth, int screenHeight)
+        protected ElementContainer _container;
+
+        protected BaseGameObject(ElementContainer container)
+        {
+            _container = container;
+        }
+
+        public abstract (List<BaseGraphicElement> graphicElements, List<StaticText> textElements) LoadContent();
+
+        public StaticText AddText(SpriteFont font, string text, Color color, int scale, TextAlignment textAlignment)
         {
             var position = Vector2.One;
 
             switch (textAlignment)
             {
                 case TextAlignment.HORIZONTALLY_AND_VERTICALLY_CENTERED:
-                    position.X = (screenWidth - font.MeasureString(text).X) / 2;
-                    position.Y = (screenHeight - font.MeasureString(text).Y) / 2;
+                    position.X = (_container.Window_Width - font.MeasureString(text).X) / 2;
+                    position.Y = (_container.Window_Height - font.MeasureString(text).Y) / 2;
                     break;
             }
 
