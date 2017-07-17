711e578814c751f36de932bf8b7ad3c843ac9686
Jarred Capellman <jcapellman@hotmail.com>
2017-07-17 18:25:33 -0500

    Change: Removed the hard coded X positions in the PartyMemberObject

--- src/MysticChronicles.Engine/GameObjects/Characters/PartyMemberObject.cs
+++ src/MysticChronicles.Engine/GameObjects/Characters/PartyMemberObject.cs
@@ -13,22 +13,24 @@ namespace MysticChronicles.Engine.GameObjects.Characters
     {
         private readonly int _partyIndex;
         private readonly string _className;
+        private int _currentHP;
+        private int _maxHP;
 
-        public PartyMemberObject(string name, string className, int partyIndex)
+        public PartyMemberObject(string name, string className, int partyIndex, ElementContainer container) : base(container)
         {
             Name = name;
             _partyIndex = partyIndex;
             _className = className;
         }
         
-        public override (List<BaseGraphicElement> graphicElements, List<StaticText> textElements) LoadContent(ElementContainer container)
+        public override (List<BaseGraphicElement> graphicElements, List<StaticText> textElements) LoadContent()
         {
-            var graphicElements = new List<BaseGraphicElement> {new PartyMember(container, $"characters/{_className}", _partyIndex)};
+            var graphicElements = new List<BaseGraphicElement> {new PartyMember(_container, $"characters/{_className}", _partyIndex)};
 
             var textElements = new List<StaticText>
             {
-                AddText(container.Font, Name, Color.White, 525, 510, 1),
-                AddText(container.Font, "HP 100/200", Color.White, 1000, 510, 1)
+                AddText(_container.Font, Name, Color.White, 525, 510 + (_partyIndex * 50), 1),
+                AddText(_container.Font, $"HP {_currentHP}/{_maxHP}", Color.White, 1000, 510 + (_partyIndex * 50), 1)
             };
 
             return (graphicElements, textElements);
