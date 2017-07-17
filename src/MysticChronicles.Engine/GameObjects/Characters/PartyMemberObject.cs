0a2977439fefad799e30567b446e7828ef226564
Jarred Capellman <jcapellman@hotmail.com>
2017-07-17 17:18:33 -0500

    Added: Text Elements to the PartyMemberObject

--- src/MysticChronicles.Engine/GameObjects/Characters/PartyMemberObject.cs
+++ src/MysticChronicles.Engine/GameObjects/Characters/PartyMemberObject.cs
@@ -5,6 +5,8 @@ using MysticChronicles.Engine.Objects.Element;
 using MysticChronicles.Engine.Objects.Element.Static;
 using MysticChronicles.Engine.Objects.Element.Character;
 
+using Microsoft.Xna.Framework;
+
 namespace MysticChronicles.Engine.GameObjects.Characters
 {
     public class PartyMemberObject : BaseCharacterObject
@@ -22,9 +24,13 @@ namespace MysticChronicles.Engine.GameObjects.Characters
         public override (List<BaseGraphicElement> graphicElements, List<StaticText> textElements) LoadContent(ElementContainer container)
         {
             var graphicElements = new List<BaseGraphicElement> {new PartyMember(container, $"characters/{_className}", _partyIndex)};
-            
-            var textElements = new List<StaticText>();
-            
+
+            var textElements = new List<StaticText>
+            {
+                AddText(container.Font, Name, Color.White, 525, 510, 1),
+                AddText(container.Font, "HP 100/200", Color.White, 1000, 510, 1)
+            };
+
             return (graphicElements, textElements);
         }
     }
