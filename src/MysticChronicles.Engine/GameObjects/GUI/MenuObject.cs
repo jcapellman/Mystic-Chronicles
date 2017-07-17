55baabc7e3b129c4a6be1f89281aa17bfa004cef
Jarred Capellman <jcapellman@hotmail.com>
2017-07-17 18:27:00 -0500

    Added: Initial work on the MenuObject

---
+++ src/MysticChronicles.Engine/GameObjects/GUI/MenuObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+using Microsoft.Xna.Framework;
+
+using MysticChronicles.Engine.Objects.Common;
+using MysticChronicles.Engine.Objects.Element;
+using MysticChronicles.Engine.Objects.Element.Static;
+
+namespace MysticChronicles.Engine.GameObjects.GUI
+{
+    public class MenuObject : BaseGameObject
+    {
+        public override (List<BaseGraphicElement> graphicElements, List<StaticText> textElements) LoadContent()
+        {
+            var graphicElements = new List<BaseGraphicElement> { new BackgroundImage(_container, "UI/MainMenu") };
+
+            var textElements = new List<StaticText>
+            {
+                AddText(_container.Font, "Press any key to continue", Color.White, 1, Enums.TextAlignment.HORIZONTALLY_AND_VERTICALLY_CENTERED)
+            };
+
+            return (graphicElements, textElements);
+        }
+
+        public MenuObject(ElementContainer container) : base(container)
+        {
+        }
+    }
+}
