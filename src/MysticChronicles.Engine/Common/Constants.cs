9d9099113bf7f2525bf956ca754b43ea61597c12
Jarred Capellman <jcapellman@hotmail.com>
2017-07-17 20:41:27 -0500

    Added: Initial work on the SQLite Interface

--- src/MysticChronicles.Engine/Common/Constants.cs
+++ src/MysticChronicles.Engine/Common/Constants.cs
@@ -4,6 +4,8 @@
     {
         public const string GAME_NAME = "Mystic Chronicles";
 
+        public const string SQLITE_DB_NAME = "game.db";
+
         public const int RESOLUTION_WIDTH = 1920;
 
         public const int RESOLUTION_HEIGHT = 1080;
---
+++ src/MysticChronicles.Engine/DB/Tables/BaseTable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MysticChronicles.Engine.DB.Tables
+{
+    public class BaseTable
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        public DateTime Modified { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public bool Active { get; set; }
+    }
+}
