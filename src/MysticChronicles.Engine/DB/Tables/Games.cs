9594adde6164bfe725011c190de72f131be5fa24
Jarred Capellman <jcapellman@hotmail.com>
2017-07-20 21:07:51 -0500

    Added: Games Table

---
+++ src/MysticChronicles.Engine/DB/Tables/Games.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MysticChronicles.Engine.DB.Tables
+{
+    public class Games : BaseTable
+    {
+        public DateTime SaveDate { get; set; }
+
+        public int NumberOfCredits { get; set; }
+
+        public string Location { get; set; }
+
+        public TimeSpan GameTime { get; set; }
+    }
+}
