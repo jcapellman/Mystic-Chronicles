abbc303db9f7d67bade9bf9831669ae3cf916ba4
Jarred Capellman <jcapellman@hotmail.com>
2017-07-21 18:59:21 -0500

    Added: GameVariables Table Class

---
+++ src/MysticChronicles.Engine/DB/Tables/GameVariables.cs
@@ -0,0 +1,11 @@
+﻿namespace MysticChronicles.Engine.DB.Tables
+{
+    public class GameVariables
+    {
+        public int GameID { get; set; }
+
+        public string VarName { get; set; }
+
+        public string VarValue { get; set; }
+    }
+}
