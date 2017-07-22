a60def84a45d9160a2536675d90ccf9f908351b3
Jarred Capellman <jcapellman@hotmail.com>
2017-07-21 21:54:37 -0500

    Added: Items Table

---
+++ src/MysticChronicles.Engine/DB/Tables/Items.cs
@@ -0,0 +1,13 @@
+﻿namespace MysticChronicles.Engine.DB.Tables
+{
+    public class Items
+    {
+        public string Description { get; set; }
+
+        public int Cost { get; set; }
+
+        public int HPIncrease { get; set; }
+
+        public int MPIncrease { get; set; }
+    }
+}
