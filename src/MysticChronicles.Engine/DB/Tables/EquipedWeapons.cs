bd0f50cebe176732c14cd965369cff40d4f49dcd
Jarred Capellman <jcapellman@hotmail.com>
2017-07-22 12:12:17 -0500

    Added: EquipedWeapons Table Class

---
+++ src/MysticChronicles.Engine/DB/Tables/EquipedWeapons.cs
@@ -0,0 +1,11 @@
+﻿namespace MysticChronicles.Engine.DB.Tables
+{
+    public class EquipedWeapons : BaseTable
+    {
+        public int PartyMemberID { get; set; }
+
+        public int WeaponID { get; set; }
+
+        public int SlotNumber { get; set; }
+    }
+}
