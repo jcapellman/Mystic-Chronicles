c89ccdd6612448c32ea6f2e3e8cfb3fe1426b8ff
Jarred Capellman <jcapellman@hotmail.com>
2017-07-23 12:56:32 -0500

    Added: Initial work on the GameManager Class

---
+++ src/MysticChronicles.Engine/Managers/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+
+using MysticChronicles.Engine.DB;
+using MysticChronicles.Engine.DB.Tables;
+
+namespace MysticChronicles.Engine.Managers
+{
+    public class GameManager
+    {
+        private Games _game;
+        
+        public async Task<bool> LoadGame(int gameID)
+        {
+            using (var dbManager = new DBManager())
+            {
+                var game = await dbManager.SelectOneAsync<Games>(a => a.ID == gameID);
+
+                if (game == null)
+                {
+                    return false;
+                }
+
+                _game = game;
+
+                return true;
+            }
+        }
+    }
+}
