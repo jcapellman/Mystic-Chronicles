f27d0450d547ef6e4c629f52cd48ad6053492c94
Jarred Capellman <jcapellman@hotmail.com>
2017-07-24 21:01:15 -0500

    Added: Weapons Table

---
+++ src/MysticChronicles.Engine/DB/Tables/Weapons.cs
@@ -0,0 +1,11 @@
+﻿namespace MysticChronicles.Engine.DB.Tables
+{
+    public class Weapons
+    {
+        public string Name { get; set; }
+
+        public int ATK { get; set; }
+
+        public int Cost { get; set; }
+    }
+}
