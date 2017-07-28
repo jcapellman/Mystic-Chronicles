5fba4c090ae26dc9ba5f4ecce6255050e53288d3
Jarred Capellman <jcapellman@hotmail.com>
2017-07-27 21:47:28 -0500

    Added: LoadGameState

---
+++ src/MysticChronicles.Engine/GameStates/LoadGameState.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Input.Touch;
+
+using MysticChronicles.Engine.Managers;
+using MysticChronicles.Engine.Objects.Common;
+
+namespace MysticChronicles.Engine.GameStates
+{
+    public class LoadGameState : BaseGameState
+    {
+        public LoadGameState(GameStateContainer container) : base(container)
+        {
+        }
+
+        public override void HandleInput(GamePadState gamePadState, KeyboardState keyboardState, TouchCollection touchCollection)
+        {
+            
+        }
+
+        public override async void LoadContent()
+        {
+            gContainer = await GameManager.LoadGame(1);
+        }
+    }
+}
