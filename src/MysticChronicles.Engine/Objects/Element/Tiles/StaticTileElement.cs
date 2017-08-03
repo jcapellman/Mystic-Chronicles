95552473cdb67bb022157c34df9aa1b551c08c09
Jarred Capellman <jcapellman@hotmail.com>
2017-08-03 08:25:03 -0500

    Added: Initial work on the StaticTileElement

---
+++ src/MysticChronicles.Engine/Objects/Element/Tiles/StaticTileElement.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+using MysticChronicles.Engine.Objects.Common;
+
+namespace MysticChronicles.Engine.Objects.Element.Tiles
+{
+    public class StaticTileElement : BaseGraphicElement
+    {
+        public StaticTileElement(ElementContainer elementContainer, string textureName, bool isVisible) : base(elementContainer, textureName, isVisible)
+        {
+        }
+
+        public override void Render(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(TextureElement, new Rectangle(0, 0, TextureElement.Width, TextureElement.Height), Color.White);
+        }
+    }
+}
