ae19b54de7f6399275e6b22e2b647b493e348f22
Jarred Capellman <jcapellman@hotmail.com>
2017-08-11 22:52:56 -0500

    Change: Removed the older Engine code

--- src/MysticChronicles.Engine/Enums/TextAlignment.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MysticChronicles.Engine.Enums
-{
-    public enum TextAlignment
-    {
-        HORIZONTALLY_CENTERED,
-        VERTICALLY_CENTERED,
-        HORIZONTALLY_AND_VERTICALLY_CENTERED
-    }
-}
